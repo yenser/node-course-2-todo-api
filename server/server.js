4e55be44db5c12e15cfe467eab724ae9b32ed3d2
yenser <jens.streck@gmail.com>
2017-07-25 10:44:00 -0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000 ;
 
 app.use(bodyParser.json());
 
